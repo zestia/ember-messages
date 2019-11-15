fd55e446a13705b5587c450ef119241d736892e1
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-11-15 20:10:49 +0000

    remove old routes

--- tests/dummy/app/routes/application.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-  activate() {
-    this._super(...arguments);
-    this.controllerFor('application').set('showLayout', true);
-    document.querySelector('body').classList.add('has-layout');
-  }
-});
