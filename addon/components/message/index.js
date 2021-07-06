2288827b5d8759c978b3dc860069a2aa8ae4e312
Andrew Kirwin <andrew.kirwin@zestia.com>
2021-07-06 21:48:21 +0100

    fix tests

--- addon/components/message/index.js
+++ addon/components/message/index.js
@@ -1,19 +1,27 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { defer } from 'rsvp';
 
 export default class MessageComponent extends Component {
+  willAnimate = defer();
+
   @tracked isDismissed = false;
 
   @action
   handleClickDismiss() {
     this.isDismissed = true;
+
+    this._waitForAnimation().then(() => this.args.onDismiss());
   }
 
   @action
   handleAnimationEnd() {
-    if (this.isDismissed) {
-      this.args.onDismiss?.();
-    }
+    this.willAnimate.resolve();
+  }
+
+  _waitForAnimation() {
+    this.willAnimate = defer();
+    return this.willAnimate.promise;
   }
 }
