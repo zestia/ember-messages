ceea506260e0a2b84d5e4c7afd274bd86b96b084
Andrew Kirwin <andrew.kirwin@gmail.com>
2021-08-27 21:47:39 +0100

    clean up

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,8 +1,12 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class ApplicationController extends Controller {
-  @tracked messageDismissed = false;
+  @tracked showMessage = true;
 
-  dismissMessage = () => (this.messageDismissed = true);
+  @action
+  dismissMessage() {
+    this.showMessage = false;
+  }
 }
