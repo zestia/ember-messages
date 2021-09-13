9691fe0cd758387edb0f4a7b8499c013ef17043c
Andrew Kirwin <andrew.kirwin@zestia.com>
2021-09-13 14:09:55 +0100

    update demo app

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -5,8 +5,9 @@ export default class ApplicationRoute extends Route {
   @inject('flash-message') flashMessageService;
 
   beforeModel() {
-    this.flashMessageService.add('example', 'Flash message one');
-    this.flashMessageService.add('example', 'Flash message two');
-    this.flashMessageService.add('example', 'Flash message three');
+    this.flashMessageService.add('info', 'Info message');
+    this.flashMessageService.add('success', 'Success message');
+    this.flashMessageService.add('warning', 'Warning message');
+    this.flashMessageService.add('error', 'Error message');
   }
 }
